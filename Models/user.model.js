ab11b4b45f119e5c06ed16117ecfc535f90832cf
Asim <mohdasim.srk@gmailcom>
2023-10-21 20:03:14 +0530

    backend complete

---
+++ Models/user.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+// Defining the user schema
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    savedRecipes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "recipe",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+// Creating a User model from the user schema
+const UserModel = mongoose.model("user", userSchema);
+
+// Defining Blacklist Schema
+const blacklistSchema = mongoose.Schema({
+  token: { type: String },
+});
+
+// Creating Blacklist Model Constructor
+const BlacklistModel = mongoose.model("blacklist", blacklistSchema);
+
+// Exporting the User model and Blacklist model for use in other modules
+module.exports = { UserModel, BlacklistModel };
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { connection } = require("./Config/db");
+const { userRouter } = require("./Routes/user.routes");
+const { recipeRouter } = require("./Routes/recipe.routes");
+const { authenticate } = require("./Middleware/Auth");
+app.use(express.json());
+app.use(cors());
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+app.use("/user", userRouter);
+// app.use(authenticate);
+app.use("/recipe", recipeRouter);
+
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("Server is Connected to Database Succesfully");
+  } catch (error) {
+    console.log(error.message);
+  }
+  console.log(`Server is Connected to Port:${PORT}`);
+});
